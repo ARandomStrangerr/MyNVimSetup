6aaf85fdb58e82fdabdd25db609ce09896ca7da3
T <36683300+ARandomStrangerr@users.noreply.github.com>
2024-10-08 22:43:16 -0700

    Add files via upload

--- init.lua
+++ init.lua
@@ -19,12 +19,13 @@ vim.keymap.set('n', '<C-Left>', ':tabprevious<CR>', {}); -- set previous tab whi
 vim.keymap.set('i', '<C-Left>', ':tabprevious<CR>', {}); -- set previous tab while in edit mode
 vim.api.nvim_set_keymap('i', '<Tab>', 'coc#pum#visible() ? coc#pum#confirm() : "<Tab>"', {expr=true, noremap=true, silent=true}); -- tab to confirm auto suggest
 vim.api.nvim_set_keymap('i', '<CR>', 'v:lua.enterKeyStroke()', {expr=true, noremap=true, silent=true}); -- handle shortcut for enter key
+vim.api.nvim_set_keymap('i', '<S-CR>', 'v:lua.newLine()', {expr=true, noremap=true, silenttrue}); -- create new line with semicolone at the end of current line
 vim.api.nvim_set_keymap('i', '<BS>', 'v:lua.backspaceKeyStroke()', {expr=true, noremap=true, silent=true}) -- handle shortcut for backspace key
 vim.api.nvim_set_keymap('i', '<Del>', 'v:lua.deleteKeyStroke()', {expr=true, noremap=true, silent=true}) -- handle shortcut for delete key
 vim.api.nvim_set_keymap('i', '(', '()<Left>', {noremap=true, silent=true}); -- auto complete open and close brace
 vim.api.nvim_set_keymap('i', '[', '[]<Left>', {noremap=true, silent=true}); -- auto complete open and close square brace
 vim.api.nvim_set_keymap('i', '{', '{}<Left>', {noremap=true, silent=true}); -- auto complete open and close curly brace
-vim.api.nvim_set_keymap('i', '"', 'v:lua.isNextChar(\'\"\') ? "<Right>" : \'""<Left>\'', {expr=true, noremap=true, silent=true}); -- auto complete open and close quote
+vim.api.nvim_set_keymap('i', '"', 'v:lua.isNextChar(\'\"\') ? "<Right>" : \'""<Left>\'', {expr=true, noremap=true, silent=true}); -- auto complete open and close
 vim.api.nvim_set_keymap('i', "'", "v:lua.isNextChar(\"\'\") ? '<Right>' : \"''<left>\"", {expr=true, noremap=true, silent=true}); -- auto complete open and close single quote
 vim.api.nvim_set_keymap('i', ')', "v:lua.isNextChar(')') ? '<Right>' : ')'", {expr=true, noremap=true, silent=true}); -- move out when close brace
 vim.api.nvim_set_keymap('i', ']', "v:lua.isNextChar(']') ? '<Right>' : ']'", {expr=true, noremap=true, silent=true}); -- move out when close square brace
